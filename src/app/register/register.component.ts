067853d7234172293386301c266dc716de3687e8
briankostar <brian.kostar@gmail.com>
2017-03-17 00:04:41 -0400

    .

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { UserService } from '../shared/services';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+	authType: String = '';
+	title: String = '';
+	isSubmitting: boolean = false;
+	// errors: Errors = new Errors();
+	authForm: FormGroup;
 
-  constructor() { }
+  constructor(
+	  private route: ActivatedRoute,
+	  private router: Router,
+	  private userService: UserService,
+	  private fb: FormBuilder
+  ) {
+	  // use FormBuilder to create a form group
+	  this.authForm = this.fb.group({
+		  'email': ['', Validators.required],
+		  'password': ['', Validators.required]
+	  });
+  }
 
   ngOnInit() {
   }
+
+	submitForm() {
+		this.isSubmitting = true;
+		// this.errors = new Errors();
+
+		let credentials = this.authForm.value;
+		this.userService
+			.attemptAuth(this.authType, credentials)
+			.subscribe(
+				data => this.router.navigateByUrl('/'),
+				err => {
+					// this.errors = err;
+					this.isSubmitting = false;
+				}
+			);
+	}
 
 }
--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+// import { SharedModule } from '../shared';
 import { RegisterComponent } from './register.component';
 
 const ReigsterRoute:ModuleWithProviders = RouterModule.forChild( [ {
@@ -12,6 +13,7 @@ const ReigsterRoute:ModuleWithProviders = RouterModule.forChild( [ {
 	imports     :[
 		CommonModule,
 		ReigsterRoute
+		// SharedModule
 	],
 	declarations:[ RegisterComponent ]
 } )
